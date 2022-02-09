2e6dba9e03b7d4f7052eae0324d67592dc4ebbcf
katroll <kroll256@GMAIL.COM>
2022-02-09 11:03:27 -0500

    signin stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
-            render json: user, status: :created
+            render json: {session: user.id, user: user}
+            # render json: user, status: :created
         else
             render json: { error: "Invalid username or password" }, status: :unauthorized
         end
