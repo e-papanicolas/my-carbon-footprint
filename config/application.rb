2463d08ba61217337ac5d610acb6ced6d8a411c3
katroll <kroll256@GMAIL.COM>
2022-02-09 15:39:40 -0500

    cookies

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,6 @@ module MyCarbonFootprintApi
 
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-    config.action_dispatch.cookies_same_site_protection = :strict
+
   end
 end
